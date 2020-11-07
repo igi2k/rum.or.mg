d0bc623c9afdce73d6ef165ae49a9fb8992df6fa
IGI2k <pavel.nejedly@atlas.cz>
2020-11-07 22:45:33 +0100

    svg map

--- app/map/spirit-map.jsx
+++ app/map/spirit-map.jsx
@@ -8,17 +8,19 @@ import "./spirit-map.less";
 const url = "https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json";
 
 const Colors = {
-    InnerBorder: "#A0A0A0",
-    Border: "#A0A0A0",
+    InnerBorder: "#FFFFFF",
+    Border: "#C0C0C0",
     Selected: "#FF0000"
 };
 
 export function SpiritMap() {
-    let context;
-    let width = 975 * 2;
-    let height;
-    
-    let path;
+    const outline = ({ type: "Sphere" });
+    const projection = d3.geoEqualEarth();
+    const path = d3.geoPath(projection);
+
+    const width = 975;
+    const height = getHeight(projection, outline, width); // this will change projection
+
     let mapData;
     let countries;
     
@@ -26,51 +28,52 @@ export function SpiritMap() {
         oninit: async () => {
             mapData = await m.request(url);
             countries = topojson.feature(mapData, mapData.objects.countries);
-            const projection = d3.geoEqualEarth();
-            
-            height = getHeight(projection, width); // this will change projection
-            path = d3.geoPath(projection, context);
         },
-        oncreate: (vnode) => {
-            const canvas = vnode.dom;
-            context = canvas.getContext("2d");
-        },
-        onupdate: (vnode) => {
+        view: ({ attrs }) => {
             if (!countries) {
                 return;
             }
-            
-            const selected = new Set((vnode.attrs.selected || []).map((countryCode) => 
+            const selected = new Set((attrs.selected || []).map((countryCode) => 
                 CountryCodes.get(countryCode)
             ));
-    
-    
-            context.fillStyle = Colors.Selected;
-            context.strokeStyle = Colors.Border;
-            for (const country of countries.features) {
-                context.beginPath();
-                path(country);
-                if (selected.has(country.properties.name)) {
-                    context.fill();
+            const pathCountries = countries.features.map((feature) => {
+                const name = feature.properties.name;
+                let color;
+                if (selected.has(name)) {
+                    color = Colors.Selected;
                 } else {
-                    context.stroke();
+                    color = Colors.Border;
                 }
-            }
-            // draw inner borders
-            context.strokeStyle = Colors.InnerBorder;
-            context.beginPath();
-            path(topojson.mesh(mapData, mapData.objects.countries, (a, b) => a !== b));
-            // path(topojson.mesh(mapData));
-            context.stroke();
-        },
-        view: () => (
-            <canvas width={ width } height={ height }></canvas>
-        )
+                return (
+                    <path fill={ color } d={ path(feature) } key={ name }>
+                        <title>{ name }</title>
+                    </path>
+                );
+            });
+            const innerBordersPath = path(topojson.mesh(mapData, mapData.objects.countries, (a, b) => a !== b));
+            return renderCountries(width, height, path(outline), pathCountries, innerBordersPath);
+        }
     };
 }
 
-function getHeight(projection, width) {
-    const outline = ({ type: "Sphere" });
+function renderCountries(width, height, outlinePath, pathCountries, innerBordersPath) {
+    return m(`svg[viewBox='0 0 ${width} ${height}'][xmlns='http://www.w3.org/2000/svg'][xmlns:xlink='http://www.w3.org/1999/xlink']`,[
+        m("defs", [
+            m("path", { id: "outline", d: outlinePath }),
+            m("clipPath", { id: "clip" }, [
+                m("use", { "xlink:href": new URL("#outline", location) })
+            ])
+        ]),
+        m("g", { clipPath: `url(${new URL("#clip", location)})` }, [
+            m("use", { "xlink:href": new URL("#outline", location), fill: "white" }),
+            m("g", pathCountries),
+            m("path", { fill: "none", stroke: Colors.InnerBorder, strokeLinejoin: "round", d: innerBordersPath })
+        ]),
+        m("use", { "xlink:href": new URL("#outline", location), fill: "none", stroke: Colors.Border })
+    ]);
+}
+
+function getHeight(projection, outline, width) {
     const [[x0, y0], [x1, y1]] = d3.geoPath(projection.fitWidth(width, outline)).bounds(outline);
     const dy = Math.ceil(y1 - y0), l = Math.min(Math.ceil(x1 - x0), dy);
     projection.scale(projection.scale() * (l - 1) / l).precision(0.2);
